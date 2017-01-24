f60079093660577195168b45bf36e09ef0936d5e
Eduardo de Lima Ramos <ramos.eduardo87@gmail.com>
2017-01-24 16:43:15 -0200

    Added seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,18 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+user = User.create!(
+  email:                 'admin@example.com',
+  first_name:            'admin',
+  last_name:             'system',
+  password:              'supersecret',
+  password_confirmation: 'supersecret',
+  two_factor_secret:     'v6nasf4kfe45qxbq'
+)
+
+puts 'Created `admin` user:'
+puts "Email:    #{user.email}"
+puts "Password: #{user.password}"
+puts
+puts "Google Authenticator time based two_factor_secret (spaces optional): #{user.two_factor_secret.scan(/.{4}/).join(' ')}"
+puts
